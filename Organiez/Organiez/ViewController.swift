f151e6dce8605e8b9907f3d6f601e5f8a7000994
Manos Psarakis <psarakism@gmail.com>
2017-11-25 16:49:36 +0200

    Xcode initial commit

---
+++ Organiez/Organiez/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Organiez
+//
+//  Created by m.psarakis on 25/11/2017.
+//  Copyright © 2017 m.psarakis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
